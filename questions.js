c7c6bd96b8055b223ade2f8484358873ea908641
Łukasz Nowak <34453569+LukeNo@users.noreply.github.com>
2018-02-14 20:16:00 +0100

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,92 @@
+var questions = [
+    {
+      "question": "Who was the father of Romulus and Remus?" ,
+        "option1": "Mars",
+        "option2": "Jupiter",
+        "option3": "King Numitor",
+        "option4": "The wollf",
+        "answer": "1"
+    },
+    
+    {
+      "question": "Which was the first Roman road?" ,
+        "option1": "Via Valeria",
+        "option2": "Via Flamina",
+        "option3": "Via Appia",
+        "option4": "Via Egnatia",
+        "answer": "3"
+    },
+    
+    {
+      "question": "Which enemy city did the Romans plough with salt?" ,
+        "option1": "Corinth",
+        "option2": "Syracuse",
+        "option3": "Carthage",
+        "option4": "None",
+        "answer": "4"
+    },
+    
+    {
+      "question": "Which of the following did not defeat Mithridates VI of Pontus?" ,
+        "option1": "Sulla",
+        "option2": "Marius",
+        "option3": "Lucullus",
+        "option4": "Pompey",
+        "answer": "2"
+    },
+    
+    {
+      "question": "How many times was Rome sacked in antiquity?" ,
+        "option1": "Once",
+        "option2": "Twice",
+        "option3": "Three or four",
+        "option4": "Six",
+        "answer": "3"
+    },
+    
+    {
+      "question": "Which of the following did NOT die in battle in Mesopotamia?" ,
+        "option1": "Valerian",
+        "option2": "Gordian III",
+        "option3": "Julian",
+        "option4": "Crassus",
+        "answer": "1"
+    },
+    
+    {
+      "question": "In what year did the Western Roman Empire come to an end?" ,
+        "option1": "476 CE",
+        "option2": "19 CE",
+        "option3": "251 CE",
+        "option4": "44 BCE",
+        "answer": "1"
+    },
+    
+    {
+      "question": "Which one of these was not implicated in the assassination of Julius Caesar?" ,
+        "option1": "Nero",
+        "option2": "Marc Anthony",
+        "option3": "Cassius",
+        "option4": "Brutus",
+        "answer": "1"
+    },
+    
+    {
+      "question": "When did Julius Caesar die?" ,
+        "option1": "476 CE",
+        "option2": "22 BCE",
+        "option3": "44 CE",
+        "option4": "44 BCE",
+        "answer": "4"
+    },
+    
+    {
+      "question": "The ancient city of Rome was built on this many hills:" ,
+        "option1": "One",
+        "option2": "Seven",
+        "option3": "Twelve",
+        "option4": "Three",
+        "answer": "2"
+    },
+    
+];
